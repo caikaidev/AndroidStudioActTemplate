41a1920f1bae94ab10216d80f739fa08f29cfca8
GitHub Action <action@github.com>
2022-07-20 09:55:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fccaikai/androidstudioacttemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fccaikai.androidstudioacttemplate.services
+
+import com.github.fccaikai.androidstudioacttemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/fccaikai/androidstudioacttemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fccaikai/androidstudioacttemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fccaikai.androidstudioacttemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fccaikai.androidstudioacttemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
